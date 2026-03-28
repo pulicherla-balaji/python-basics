bd24f1a91e92d8828919a145dfdf541cad0977e7
pulicherla-balaji <pulicherlabalaji154@gmail.com>
2026-03-28 10:54:36 +0530

    Add number comparison script
    Add a script to compare two numbers and print their relationship.

---
+++ largest among 3 nums.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number: ") )
+b=int(input("Enter another number: ") ) 
+if a>b:
+    print("The first number is greater than the second number.")
+elif a<b:
+    print("The first number is less than the second number.")
+else:   
+    print("The first number is equal to the second number.")
